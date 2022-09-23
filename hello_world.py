b7a9e02704fd803acfa14ab881fa34c2ba6be7f9
Sharpa1215 <hifriend333@naver.com>
2022-09-23 09:27:53 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ def print_hello():
     print("Hello World!!")
 
 def print_KIT():
-    print("Kumoh National Institute of Technology.")
+    print("Kumoh Natiol Institute of Technology.")
 
 if __name__ == '__main__':
     print_KIT()
